b8b24ef0432531ba5952bf5da2ef31fef1692217
mahmoooodddd <mahmoudmohsenifar@gmail.com>
2020-07-18 19:12:57 +0430

    change Download model

--- app/Download.php
+++ app/Download.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Download extends Model
 {
     protected $fillable = ['name','filepath','price'];
+
+
+        public function user()
+    {
+
+        return $this->belongsTo('App\User');
+    }
+
+    public function order()
+
+    {
+
+        return $this->belongsTo('App\Order');
+    }
+
+
+
 }
