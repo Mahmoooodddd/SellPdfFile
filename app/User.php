adcaacc35cf13e38b1432ca6cc342b6a04e48e50
mahmoooodddd <mahmoudmohsenifar@gmail.com>
2020-08-01 16:18:56 +0430

    change to first step

--- app/User.php
+++ app/User.php
@@ -37,13 +37,4 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function orders()
-    {
-        return $this->hasMany('App\Order');
-
-    }
-    public function downloads()
-    {
-        return $this->hasMany('App\Download');
-    }
 }
