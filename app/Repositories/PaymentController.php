b4c36e22d26423a70207464ab23a9a00c67daf77
mahmoooodddd <mahmoudmohsenifar@gmail.com>
2020-08-17 11:35:23 +0430

    create payments table

---
+++ app/Repositories/PaymentController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mahmood
+ * Date: 8/17/20
+ * Time: 11:15 AM
+ */
+
+namespace App\Repositories;
+
+
+class PaymentController
+{
+
+}
---
+++ app/Services/PaymentService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mahmood
+ * Date: 8/17/20
+ * Time: 11:16 AM
+ */
+
+namespace App\Services;
+
+
+class PaymentService
+{
+
+}
